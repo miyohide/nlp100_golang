87a1b2f32b5bcd12681bcb4da2634b5da6ee961d
Hidenori Miyoshi <miyohide@gmail.com>
2018-01-18 22:44:03 +0900

    Refactoring. Extract Method

--- q03/main.go
+++ q03/main.go
@@ -5,11 +5,23 @@ import (
 	"strings"
 )
 
+func CountWords(s string) ([]string, []int) {
+	wordlens := []int{}
+	s = strings.Replace(s, ",", "", -1)
+	s = strings.Replace(s, ".", "", -1)
+
+	words := strings.Split(s, " ")
+	for i := 0; i < len(words); i++ {
+		wordlens = append(wordlens, len(words[i]))
+	}
+	return words, wordlens
+}
+
 func main() {
 	s := "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-	words := strings.Split(s, " ")
+	words, lens := CountWords(s)
 
 	for i := 0; i < len(words); i++ {
-		fmt.Printf("%s %d\n", words[i], len(words[i]))
+		fmt.Printf("%s %d\n", words[i], lens[i])
 	}
 }
